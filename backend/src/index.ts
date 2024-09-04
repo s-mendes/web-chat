69b483c37e156c7fbbba62856e8671c61327e124
Samuel Mendes <samuelpanzer@hotmail.com>
2024-09-03 22:36:04 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,8 @@ const wss = new WebSocketServer({ port: parseInt(process.env.PORT || "8080") });
 wss.on("connection", (ws) => {
   ws.on("error", console.error);
 
+  console.log("WebSocket connection established");
+
   ws.on("message", (message) => {
     wss.clients.forEach((client) => {
       client.send(message.toString());
